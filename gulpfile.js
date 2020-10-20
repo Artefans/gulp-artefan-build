47713382bd17a4cd2b519e0a7fefb1a3a0dfa677
artefan <45694209+Artefans@users.noreply.github.com>
2020-10-20 17:19:30 +0300

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,7 @@ const paths = require('./gulp/paths');
 
 const serve = () => {
   return browserSync.init({
-    server: 'build',
+    server: 'dist',
     notify: false,
     open: false,
     cors: true,
